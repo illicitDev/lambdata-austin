b4515637616652a3f8684f8f413a5a3d01e930b4
illicitDev <afrancis2345@gmail.com>
2021-01-11 14:14:03 -0800

    updated docstrings

--- lambdata-austin/__init__.py
+++ lambdata-austin/__init__.py
@@ -2,4 +2,4 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 
-'''Exploration of python libraries, packeges, and methods.'''
+'''Exploration of python libraries, packages, and methods.'''
--- lambdata-austin/helper_functions.py
+++ lambdata-austin/helper_functions.py
@@ -1,7 +1,7 @@
 def null_count(df):
-    '''Return NaN value counts in passed df'''
+    '''Return NaN value counts in passed DataFrame'''
     return df.isnull().sum()
 
 def randomize(df, seed):
-    '''Randomize all cells in a df'''
+    '''Randomize all cells in a DataFrame'''
     return shuffle(df, random_state=seed)
