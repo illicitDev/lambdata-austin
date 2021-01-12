50a9941d63133b79248f81554a46b85b793d60f3
illicitDev <afrancis2345@gmail.com>
2021-01-12 13:40:29 -0800

    added class and tts method

--- lambdata-austin/__init__.py
+++ lambdata-austin/__init__.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 import numpy as np
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split as tts
 
-'''Exploration of python libraries, packages, and methods.'''
+
+"""Exploration of python libraries, packages, and methods."""
--- lambdata-austin/helper_functions.py
+++ lambdata-austin/helper_functions.py
@@ -1,7 +1,16 @@
-def null_count(df):
-    '''Return NaN value counts in passed DataFrame'''
-    return df.isnull().sum()
+class Helper:
+    def __init__(self, df):
+        self.df = df.copy()
 
-def randomize(df, seed):
-    '''Randomize all cells in a DataFrame'''
-    return shuffle(df, random_state=seed)
+    def null_count(self):
+        """Return NaN value counts in passed DataFrame"""
+        return self.df.isnull().sum().sum()
+
+    def train_test_split(self, frac):
+        """Perform a train test split"""
+        return tts(self.df, train_size=frac)
+
+    def randomize(self, seed):
+        """Randomize all cells in a DataFrame"""
+        return shuffle(self.df, random_state=seed)
+
