dbf931ee896721c919a2c7ef46c6653ebbb1edb3
Austin Francis <afrancis2345@gmail.com>
2021-01-14 15:19:49 -0800

    added unittest and updated README

--- lambdata-austin/helper_functions.py
+++ lambdata-austin/helper_functions.py
@@ -1,4 +1,5 @@
 """Exploration of python libraries, packages, and methods."""
+
 import pandas as pd 
 import numpy as np
 from sklearn.utils import shuffle
@@ -6,6 +7,7 @@ from sklearn.model_selection import train_test_split as tts
 
 
 class Helper:
+    
     def __init__(self, df):
         self.df = df.copy()
 
@@ -21,3 +23,22 @@ class Helper:
         """Randomize all cells in a DataFrame"""
         return shuffle(self.df, random_state=seed)
 
+
+if __name__ == '__main__':
+    kep = pd.read_csv('data/kep_observations.csv')
+
+    h = Helper(kep) 
+
+    train, test = h.train_test_split(.80)
+
+    kep_rnd = h.randomize(42)
+
+    print('HELPER FUNCTIONS TEST')
+    print('-------------------------------------------')
+    print('Null Count Test:', h.null_count())
+    print(kep.count())
+    print()
+    print('Train: {}, Test: {}'.format(len(train), len(test)))
+    print()
+    print(kep_rnd.head())
+    print('-------------------------------------------')
